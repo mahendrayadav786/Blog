3c5fa7c180b9652551ea9051f5f142f47ce69d02
Mahendra Yadav <mahendrayadav7571@gmail.com>
2022-01-31 21:27:53 +0530

    initialcommit

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ from flask_gravatar import Gravatar
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] =os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("postgres://vuhorgsnhkbrag:794bf68e834cc4da78b4f7c1e3022b4a0ad0a20f75e99198e78286981389ac38@ec2-54-208-139-247.compute-1.amazonaws.com:5432/d2bcqag6dmnv8b", "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] =  "sqlite:///blog.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
